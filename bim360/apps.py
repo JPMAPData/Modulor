603d570bda293c44ba1e50d1cadbd7422ad8a4ba
John Dynamo <joao.martins@mapdata.com.br>
2019-07-05 16:47:04 -0300

    sexta

---
+++ bim360/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Bim360Config(AppConfig):
+    name = 'bim360'
---
+++ bim360/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, redirect, get_object_or_404
+
+# Create your views here.
+
+def get_ids(request):
+    form = form(request, )
