6bfb77d5b5e334e417083770f91726cc656ffd76
John Dynamo <joao.martins@mapdata.com.br>
2019-07-15 09:34:13 -0300

    not working

--- bim360/urls.py
+++ bim360/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('projetos/', views.projects_list, name='projects'),
     path('pastas/', views.folders_list, name='folders'),
     path('topfolder/', views.topfolders, name='topfolders'),
-    path()
 ]
